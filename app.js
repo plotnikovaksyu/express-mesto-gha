3db453ba5ec1859759dc92e847bc37178efc60d8
Kseniya Plotnikova <plotnikova-ksyuh@yandex.ru>
2023-05-17 22:03:11 +0300

    добавить обраточик celebrate

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { errors } = require('celebrate');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -8,7 +9,7 @@ const { cardsRoter } = require('./routes/cards');
 const { limiter } = require('./middleware/rate-limiter');
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
-
+app.use(errors());
 app.use(limiter);
 app.use(express.json());
 app.use(usersRoter);
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const { celebrate, Joi } = require('celebrate');
+
 const express = require('express');
 const {
   getUsers,
@@ -8,16 +10,20 @@ const {
   login,
 } = require('../controllers/users');
 
-const {
-  createUserValidation,
-} = require('../middleware/validation');
-
 const auth = require('../middleware/auth');
 
 const usersRoter = express.Router();
 
 usersRoter.post('/signin', login);
-usersRoter.post('/signup', createUserValidation, createUser);
+usersRoter.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-0-9/a-z()@:%.+~#=_]+\.{1}[a-z0-9]+\b[//a-z0-9()@:%_+.~#?&=]*/mi),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
 
 usersRoter.use(auth);
 
