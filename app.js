6512ab9f5e2c2e3d586a890a57d3d87012c63bc6
Kseniya Plotnikova <plotnikova-ksyuh@yandex.ru>
2023-05-17 22:27:57 +0300

    изменить валидацию для созданию юзера

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
+const auth = require('./middleware/auth');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 const { usersRoter } = require('./routes/users');
 const { cardsRoter } = require('./routes/cards');
 const { limiter } = require('./middleware/rate-limiter');
+const { createUser, login } = require('./controllers/users');
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
-app.use(errors());
 app.use(limiter);
 app.use(express.json());
+
+app.post('/signin', login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string()
+      .pattern(/https?:\/\/(www\.)?[-0-9/a-z()@:%.+~#=_]+\.{1}[a-z0-9]+\b[//a-z0-9()@:%_+.~#?&=]*/mi),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
+
+app.use(auth);
 app.use(usersRoter);
 
 app.use(cardsRoter);
 
+app.use(errors());
 app.all('*', (req, res) => {
   res.status(404).send({ message: 'Запрашиваемая страница не существует' });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,3 @@
-const { celebrate, Joi } = require('celebrate');
-
 const express = require('express');
 const {
   getUsers,
@@ -7,29 +5,28 @@ const {
   createUser,
   updateUserInfo,
   updateAvatar,
-  login,
+  // login,
 } = require('../controllers/users');
-
-const auth = require('../middleware/auth');
 
 const usersRoter = express.Router();
 
-usersRoter.post('/signin', login);
-usersRoter.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-0-9/a-z()@:%.+~#=_]+\.{1}[a-z0-9]+\b[//a-z0-9()@:%_+.~#?&=]*/mi),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), createUser);
+// usersRoter.post('/signin', login);
+// usersRoter.post('/signup', celebrate({
+//   body: Joi.object().keys({
+//     name: Joi.string().required().min(2).max(30),
+//     about: Joi.string().required().min(2).max(30),
+//     avatar: Joi.string()
+// .pattern(/https?:\/\/(www\.)?[-0-9/a-z()@:%.+~#=_]+\.{1}[a-z0-9]+\b[//a-z0-9()@:%_+.~#?&=]*/mi),
+//     email: Joi.string().required().email(),
+//     password: Joi.string().required().min(8),
+//   }),
+// }), createUser);
 
-usersRoter.use(auth);
+// usersRoter.use(auth);
 
 usersRoter.get('/users', getUsers);
 usersRoter.get('/users/:userId', getUser);
-// usersRoter.post('/users', createUser);
+usersRoter.post('/users', createUser);
 usersRoter.patch('/users/me', updateUserInfo);
 usersRoter.patch('/users/me/avatar', updateAvatar);
 
