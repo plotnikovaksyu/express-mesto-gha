dcc7cfb67cebd0666f125926f4c0f6b1e40b72da
Kseniya Plotnikova <plotnikova-ksyuh@yandex.ru>
2023-05-18 20:53:43 +0300

    изменить колво в лимитере

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -1,7 +1,7 @@
 const rateLimiter = require('express-rate-limit');
 
 const limiter = rateLimiter({
-  max: 5,
+  max: 10,
   windowMS: 10000, // 10 seconds
   message: "You can't make any more requests at the moment. Try again later",
 });
