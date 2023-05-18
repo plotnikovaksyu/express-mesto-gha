02ab6f904c0c9ef6e15d2a35c676244fb0fd7b3c
Kseniya Plotnikova <plotnikova-ksyuh@yandex.ru>
2023-05-18 20:58:38 +0300

    изменить лимитер

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -1,7 +1,7 @@
 const rateLimiter = require('express-rate-limit');
 
 const limiter = rateLimiter({
-  max: 10,
+  max: 100,
   windowMS: 10000, // 10 seconds
   message: "You can't make any more requests at the moment. Try again later",
 });
